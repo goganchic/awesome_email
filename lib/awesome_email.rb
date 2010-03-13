2367e908cd05c96dac6e48698630865ef8cc9d8b
Daniel Jagszent <daniel@jagszent.de>
2010-03-13 19:28:27 +0100

    ActionMailer::Base has already got layouts (class level method) -> remove this from awsome_email

--- lib/awesome_email.rb
+++ lib/awesome_email.rb
@@ -1,4 +1,3 @@
-require File.join(File.dirname(__FILE__), 'awesome_email', 'layouts.rb')
 require File.join(File.dirname(__FILE__), 'awesome_email', 'inline_styles.rb')
 require File.join(File.dirname(__FILE__), 'awesome_email', 'convert_entities.rb')
 require File.join(File.dirname(__FILE__), 'awesome_email', 'helpers.rb')
